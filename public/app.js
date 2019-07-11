e957b5c44b3fe3829af17561a5ba285f0816a230
tuequach <tuequach@gmail.com>
2019-07-11 18:54:32 -0500

    getting ajax post request to change the note based on the inputs, logging any response and empty any needed value

--- public/app.js
+++ public/app.js
@@ -26,5 +26,31 @@ $.ajax({
     $('#notes').append("<textarea id='bodyinput' name='body'></textarea>");
     //creating button for submitting new note with id of the article and saving it
     $('#notes').append("<button data=id='" + data._id + "' id='savenote'>Save Note</button>");
+
+    //placing a note in the article based on title input and body text area
+    if (data.note) {
+        $('#titleinput').val(data.note.title);
+        $('#bodyinput').val(data.note.body);
+    };
 })
-})
+});
+
+$(document).on('click', '#savenote', function() {
+    var thisId = $(this).attr('data-id');
+
+    //creating POST request to change the note based on the inputs
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId, 
+        data: {
+            title: $('#titleinput').val(),
+            body: $('#bodyinput').val()
+        }
+    })
+    .then(function(data) {
+        console.log(data);
+        $('#notes').empty();
+    });
+    $('#titleinput').val("");
+    $('#bodyinput').val("");
+});
