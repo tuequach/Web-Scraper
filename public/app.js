a424f3fdcab99ebfbbb386d8a61641a40731904f
tuequach <tuequach@gmail.com>
2019-07-11 18:41:33 -0500

    creating ajax call for the Article and displaying the title and information to page based on the input (textarea or button)

--- public/app.js
+++ public/app.js
@@ -8,3 +8,23 @@ $.getJSON('/articles' , function (data) {
 $(document).on('click', 'p', function() {
     $('#notes').empty(); 
     var thisId = $(this).attr('data-id');
+
+    //creating ajax call for the Article
+$.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+})
+
+//adding note information to the page
+.then(function(data) {
+    console.log(data);
+    //title of the article
+    $('#notes').append("<h2>" + data.title + "</h2");
+    //input for new title
+    $('#notes').append("<input id='titleinput' name ='title' >");
+    //add new note body 
+    $('#notes').append("<textarea id='bodyinput' name='body'></textarea>");
+    //creating button for submitting new note with id of the article and saving it
+    $('#notes').append("<button data=id='" + data._id + "' id='savenote'>Save Note</button>");
+})
+})
